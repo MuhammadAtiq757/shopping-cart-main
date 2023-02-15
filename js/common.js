f238bb29de0e88554376e16e5430fbac859da0ee
MuhammadAtiq <muhammadatiq757@gamil.com>
2023-02-16 00:55:59 +0600

    first commit

---
+++ js/common.js
@@ -0,0 +1,49 @@
+function updateCaseNumber(isInCrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+   let newCaseNumber;
+   if(isInCrease === true){
+    newCaseNumber =previousCaseNumber +1;
+   }
+
+   else{
+    newCaseNumber = previousCaseNumber -1;
+   }
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+function getTextElementValueById(elementId){
+    const phoneTotalElement = document.getElementById(elementId);
+    const currentPhoneTotalString = phoneTotalElement.innerText;
+    const currentPhoneTotal = parseInt(currentPhoneTotalString);
+    return currentPhoneTotal;
+    
+    
+    
+    }
+   function setTextElementValueById(elementId, value){
+    const subTotalElement = document.getElementById(elementId);
+    subTotalElement.innerText = value; 
+    
+
+   } 
+    
+    function calculateSubTotal(){
+    // calculate phonTotal
+    const currentPhoneTotal = getTextElementValueById('phone-total');
+    const currentCaseTotal = getTextElementValueById('case-total');
+    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
+    
+setTextElementValueById('sub-total', currentSubTotal);
+
+// calculate tax
+const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+const taxAmount = parseFloat(taxAmountString);
+setTextElementValueById('tax-amount', taxAmount);
+
+const finalAmount = currentSubTotal + taxAmount;
+setTextElementValueById('final-total', finalAmount)
+    }
